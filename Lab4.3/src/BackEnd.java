b53fe77ff17c4a03dfe767dd916c888b62fac218
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-01-02 13:13:13 -0500

    .

---
+++ Lab4.3/src/BackEnd.java
@@ -0,0 +1,10 @@
+
+public class BackEnd 
+{
+
+	public BackEnd() 
+	{
+		
+	}
+
+}
---
+++ Lab4.3/src/GameCode.java
@@ -0,0 +1,53 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Labeled;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class GameCode extends Application
+{
+
+	public static void main(String[] args) 
+	{
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage) 
+	{
+        primaryStage.setTitle("Wack-a-Button");
+        Button btn = new Button();
+        btn.setText("Click Me!");
+        btn.setLayoutX(350);
+        btn.setLayoutY(350);
+               
+        btn.setOnAction(new EventHandler<ActionEvent>() 
+        {
+        	int score = 0;
+        	int randomX = 0;
+        	int randomY = 0;
+            @Override
+            public void handle(ActionEvent event) 
+            {
+            	randomX = 0 + (int)(Math.random() * 735);
+            	randomY = 0 + (int)(Math.random() * 775);
+            	btn.setLayoutX(randomX);
+                btn.setLayoutY(randomY);
+                score++;
+                System.out.println("Your Score: " + score);
+            }
+        });
+        
+        Pane root = new Pane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.show();
+    }
+
+}
